9ac579735da0f3d228fe76c2339963aa34b8638d
naudhizb <naudhizb@gmail.com>
2021-03-11 23:18:39 +0900

    Update MRAM

--- mr25h10.c
+++ mr25h10.c
@@ -7,8 +7,6 @@ static void MR25H10_ChipSelect(MR25H10_ctx_t *ctx);
 static void MR25H10_ChipDeselect(MR25H10_ctx_t *ctx);
 static void MR25H10_WriteEnable(MR25H10_ctx_t *ctx);
 static void MR25H10_WriteDisable(MR25H10_ctx_t *ctx);
-static void MR25H10_ReadStatus(MR25H10_ctx_t *ctx, uint8_t *status);
-static void MR25H10_WriteStatus(MR25H10_ctx_t *ctx, uint8_t *status);
 
 #define	OPCODE_WREN		0x06	/* Write enable */
 #define OPCODE_WRDI		0x04	/* Wrtie disable */
@@ -19,16 +17,15 @@ static void MR25H10_WriteStatus(MR25H10_ctx_t *ctx, uint8_t *status);
 #define	OPCODE_SLEEP	0xb9	/* Enter Sleep Mode */
 #define	OPCODE_WAKE		0xab	/* Exit Sleep Mode */
 
-#define STATUS_BIT_SRWD 	7
-#define STATUS_BIT_USER3 	6
-#define STATUS_BIT_USER2 	5
-#define STATUS_BIT_USER1 	4
-#define STATUS_BIT_BP1 		3
-#define STATUS_BIT_BP0 		2
-#define STATUS_BIT_WEL 		1
-#define STATUS_BIT_USER0 	0
 
-
+/**
+ * @brief Initialize MR25H10 Object
+ * 
+ * @param ctx Context Container pointer
+ * @param hspi SPI Handle(SPI shell be Mode 0 or Mode 3. MR25H10 support up to 40Mhz)
+ * @param GPIOx Chip Select Port
+ * @param GPIO_Pin Chip Select Pin
+ */
 void MR25H10_Init(MR25H10_ctx_t *ctx, SPI_HandleTypeDef *hspi,  GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin){
 	ctx->hspi = hspi;
 	ctx->GPIOx = GPIOx;
@@ -37,9 +34,17 @@ void MR25H10_Init(MR25H10_ctx_t *ctx, SPI_HandleTypeDef *hspi,  GPIO_TypeDef* GP
 	ctx->MutexHandle = osMutexCreate(osMutex(MRAMMutex));
 }
 
-void MR25H10_Read(MR25H10_ctx_t *ctx, uint32_t addr, uint8_t *pdata, uint32_t size){
+/**
+ * @brief Read MRAM
+ * 		  (*Note. It can read 0xFFFF in one time -- Half of whole memory)
+ * @param ctx MR25H10 Handle
+ * @param addr Address [0x0 0x1FFFF]
+ * @param pdata pointer of read data
+ * @param size size of read data [0x0 0xFFFF]
+ */
+int32_t MR25H10_Read(MR25H10_ctx_t *ctx, uint32_t addr, uint8_t *pdata, uint16_t size){
 	if(MR25H10_MAX_SIZE < (addr + size)){
-		return;
+		return EXIT_FAILURE;
 	}
 	osMutexWait(ctx->MutexHandle, MR25H10_TIMEOUT);
 	uint8_t cmd[4] = {
@@ -55,10 +60,20 @@ void MR25H10_Read(MR25H10_ctx_t *ctx, uint32_t addr, uint8_t *pdata, uint32_t si
 	MR25H10_ChipDeselect(ctx);
 
 	osMutexRelease(ctx->MutexHandle);
+	return EXIT_SUCCESS;
 }
-void MR25H10_Write(MR25H10_ctx_t *ctx, uint32_t addr, uint8_t *pdata, uint32_t size){
+
+/**
+ * @brief Write Data
+ * 		  (*Note. It can read 0xFFFF in one time -- Half of whole memory)
+ * @param ctx MR25H10 Handle
+ * @param addr Address [0x0 0x1FFFF]
+ * @param pdata pointer of write data
+ * @param size size of write data [0x0 0xFFFF]
+ */
+int32_t MR25H10_Write(MR25H10_ctx_t *ctx, uint32_t addr, uint8_t *pdata, uint16_t size){
 	if(MR25H10_MAX_SIZE < (addr + size)){
-		return;
+		return EXIT_FAILURE;
 	}
 	osMutexWait(ctx->MutexHandle, MR25H10_TIMEOUT);
 	uint8_t cmd[4] = {
@@ -74,10 +89,19 @@ void MR25H10_Write(MR25H10_ctx_t *ctx, uint32_t addr, uint8_t *pdata, uint32_t s
 	MR25H10_ChipDeselect(ctx);
 	MR25H10_WriteDisable(ctx);
 	osMutexRelease(ctx->MutexHandle);
+	return EXIT_SUCCESS;
 }
-void 	MR25H10_Erase(MR25H10_ctx_t *ctx, uint32_t addr, uint32_t size){
+
+/**
+ * @brief Erase Data
+ * 
+ * @param ctx MR25H10 handle
+ * @param addr address of erase data
+ * @param size size of erase data
+ */
+int32_t MR25H10_Erase(MR25H10_ctx_t *ctx, uint32_t addr, uint32_t size){
 	if(MR25H10_MAX_SIZE < (addr + size)){
-		return;
+		return EXIT_FAILURE;
 	}
 	osMutexWait(ctx->MutexHandle, MR25H10_TIMEOUT);
 	uint8_t cmd[4] = {
@@ -96,18 +120,63 @@ void 	MR25H10_Erase(MR25H10_ctx_t *ctx, uint32_t addr, uint32_t size){
 	MR25H10_ChipDeselect(ctx);
 	MR25H10_WriteDisable(ctx);
 	osMutexRelease(ctx->MutexHandle);
-
-
+	return EXIT_SUCCESS;
 }
+
+/**
+ * @brief Read one byte from MR25H10
+ * 
+ * @param ctx MR25H10 handle
+ * @param addr read data address
+ * @return uint8_t read value
+ */
 uint8_t MR25H10_ReadByte(MR25H10_ctx_t *ctx, uint32_t addr){
 	uint8_t data = 0;
 	MR25H10_Read(ctx, addr, &data, sizeof(data));
 	return data;
 }
+
+/**
+ * @brief Write one byte to MR25H10
+ * 
+ * @param ctx MR25H10 handle
+ * @param addr write data address
+ * @param data write value
+ */
 void MR25H10_WriteByte(MR25H10_ctx_t *ctx, uint32_t addr, uint8_t data){
 	MR25H10_Write(ctx, addr, &data, sizeof(data));
 	return;
 }
+
+/**
+ * @brief Read MR25H10 status
+ * 
+ * @param ctx  MR25H10 Handle
+ * @param status MR25H10 status.
+ */
+void MR25H10_ReadStatus(MR25H10_ctx_t *ctx, uint8_t *status){
+	uint8_t cmd[2] = {OPCODE_RDSR, 0};
+	MR25H10_ChipSelect(ctx);
+	HAL_SPI_TransmitReceive(ctx->hspi, cmd, cmd, sizeof(cmd), MR25H10_TIMEOUT);
+	MR25H10_ChipDeselect(ctx);
+	*status = cmd[1];
+}
+
+/**
+ * @brief Write MR25H10 status
+ * 	(Note. you don't need to use this function unless protect memory area)
+ * @param ctx MR25H10 handle
+ * @param status MR25H10 status.
+ */
+void MR25H10_WriteStatus(MR25H10_ctx_t *ctx, uint8_t *status){
+	uint8_t cmd[2] = {OPCODE_RDSR, *status};
+	MR25H10_WriteEnable(ctx);
+	MR25H10_ChipSelect(ctx);
+	HAL_SPI_TransmitReceive(ctx->hspi, cmd, cmd, sizeof(cmd), MR25H10_TIMEOUT);
+	MR25H10_ChipDeselect(ctx);
+	MR25H10_WriteDisable(ctx);
+}
+
 
 static void MR25H10_ChipSelect(MR25H10_ctx_t *ctx){
 	HAL_GPIO_WritePin(ctx->GPIOx, ctx->GPIO_Pin, GPIO_PIN_RESET);
@@ -126,21 +195,4 @@ static void MR25H10_WriteDisable(MR25H10_ctx_t *ctx){
 	MR25H10_ChipSelect(ctx);
 	HAL_SPI_Transmit(ctx->hspi, &cmd, sizeof(cmd), MR25H10_TIMEOUT);
 	MR25H10_ChipDeselect(ctx);
-}
-static void MR25H10_ReadStatus(MR25H10_ctx_t *ctx, uint8_t *status){
-	uint8_t cmd[2] = {OPCODE_RDSR, 0};
-	MR25H10_ChipSelect(ctx);
-	HAL_SPI_TransmitReceive(ctx->hspi, cmd, cmd, sizeof(cmd), MR25H10_TIMEOUT);
-	MR25H10_ChipDeselect(ctx);
-
-	*status = cmd[1];
-}
-static void MR25H10_WriteStatus(MR25H10_ctx_t *ctx, uint8_t *status){
-	uint8_t cmd[2] = {OPCODE_RDSR, *status};
-	MR25H10_WriteEnable(ctx);
-	MR25H10_ChipSelect(ctx);
-	HAL_SPI_TransmitReceive(ctx->hspi, cmd, cmd, sizeof(cmd), MR25H10_TIMEOUT);
-	MR25H10_ChipDeselect(ctx);
-	MR25H10_WriteDisable(ctx);
-
 }
--- mr25h10.h
+++ mr25h10.h
@@ -11,6 +11,7 @@
 #include "stm32f4xx_hal.h"
 #include <cmsis_os.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 #define MR25H10_TIMEOUT 	500
 #define MR25H10_MAX_SIZE 	0x20000
@@ -22,13 +23,26 @@ typedef struct MR25H10_ctx_st {
     osMutexId MutexHandle;
 } MR25H10_ctx_t;
 
+/**
+ * @brief MR25H10 status bit
+ */
+#define STATUS_BIT_SRWD 	7
+#define STATUS_BIT_USER3 	6
+#define STATUS_BIT_USER2 	5
+#define STATUS_BIT_USER1 	4
+#define STATUS_BIT_BP1 		3
+#define STATUS_BIT_BP0 		2
+#define STATUS_BIT_WEL 		1
+#define STATUS_BIT_USER0 	0
+
 void 	MR25H10_Init(MR25H10_ctx_t *ctx, SPI_HandleTypeDef *hspi,  GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
-int32_t MR25H10_Check(MR25H10_ctx_t *ctx);
-void 	MR25H10_Read(MR25H10_ctx_t *ctx, uint32_t addr, uint8_t *pdata, uint32_t size);
-void 	MR25H10_Write(MR25H10_ctx_t *ctx, uint32_t addr, uint8_t *pdata, uint32_t size);
-void 	MR25H10_Erase(MR25H10_ctx_t *ctx, uint32_t addr, uint32_t size);
+int32_t 	MR25H10_Read(MR25H10_ctx_t *ctx, uint32_t addr, uint8_t *pdata, uint16_t size);
+int32_t 	MR25H10_Write(MR25H10_ctx_t *ctx, uint32_t addr, uint8_t *pdata, uint16_t size);
+int32_t 	MR25H10_Erase(MR25H10_ctx_t *ctx, uint32_t addr, uint32_t size);
 uint8_t MR25H10_ReadByte(MR25H10_ctx_t *ctx, uint32_t addr);
 void 	MR25H10_WriteByte(MR25H10_ctx_t *ctx, uint32_t addr, uint8_t data);
+void    MR25H10_ReadStatus(MR25H10_ctx_t *ctx, uint8_t *status);
+void    MR25H10_WriteStatus(MR25H10_ctx_t *ctx, uint8_t *status);
 
 
 
